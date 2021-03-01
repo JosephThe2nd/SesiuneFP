2065a8327a7c36a56a3cad6d21726f32d7b0a05e
Covasan Iosif <covasan.iosif7@gmail.com>
2021-03-01 18:15:30 +0200

    s

--- Poo_curs1_Prob 2/Program.cs
+++ Poo_curs1_Prob 2/Program.cs
@@ -6,7 +6,17 @@ namespace Poo_curs1_Prob_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] v = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine($"Suma este {returnSum(v)}");
+        }
+        static int returnSum(int[] v)
+        {
+            int s = 0;
+            for (int i = 0; i < v.Length; i++)
+            {
+                s += i;
+            }
+            return s;
         }
     }
 }
