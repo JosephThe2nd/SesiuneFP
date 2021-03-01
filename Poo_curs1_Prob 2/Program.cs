78f10cd6f266f34e31bd4a48028013270c12d94b
Covasan Iosif <covasan.iosif7@gmail.com>
2021-03-01 18:37:02 +0200

    mod recursiv

--- Poo_curs1_Prob 2/Program.cs
+++ Poo_curs1_Prob 2/Program.cs
@@ -7,8 +7,11 @@ namespace Poo_curs1_Prob_2
         static void Main(string[] args)
         {
             int[] v = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            Console.WriteLine($"Suma este {returnSum(v)}");
+            //Console.WriteLine($"Suma este {returnSum(v)}");
+            int suma = sumRecursiv(v, 0);
+            Console.WriteLine($"Suma este {suma}");
         }
+        /*
         static int returnSum(int[] v)
         {
             int s = 0;
@@ -17,6 +20,16 @@ namespace Poo_curs1_Prob_2
                 s += i;
             }
             return s;
+        }
+        */
+        static int sumRecursiv(int[] v , int index)
+        {
+            if (index == v.Length)
+            {
+                return 0;
+            }
+            return v[index] + sumRecursiv(v , index + 1);
+
         }
     }
 }
